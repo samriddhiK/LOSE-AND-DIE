1c7bf2e64988bb12fec2a2a11062bdac92f4e20c
samriddhiK <75959569+samriddhiK@users.noreply.github.com>
2021-04-16 06:41:59 -0700

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,14 @@
+score = 0;
+function updateScore()
+{
+    score = score + 1;
+    document.getElementById("score") .innerHTML = "Score:" + score;
+}
+function saveScore()
+{
+    localStorage.setItem("score", score);
+}
+function nextPage()
+{
+window.location = "win_and_live.html"; 
+}
